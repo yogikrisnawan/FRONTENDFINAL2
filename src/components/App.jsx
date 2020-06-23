c67bdaa3da5cd16b4d07484d116c843c508789d6
sawm97 <sriariefwibawamukti@gmail.com>
2020-06-23 15:46:50 +0700

    commit sawm97

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter, Switch }  from 'react-router-dom'
 import Header from '../components/Header'
 import Home from '../components/Home'
 import NotFound from '../components/404'
+import Arief from '../components/Arief'
 
 export default function App() {
    return (
@@ -12,6 +13,7 @@ export default function App() {
             <Header/>
             <Switch>
                <Route path="/" exact component={Home}/>
+               <Route path="/arief" component={Arief} />
                <Route component={NotFound} />
             </Switch>
          </div>
