a2a182bef9f5df4e1c664005e30b7e28f77b5480
dhimasf098 <d.fadhlirrahman98@gmail.com>
2020-06-23 15:29:45 +0700

    dhimasf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter, Switch }  from 'react-router-dom'
 import Header from '../components/Header'
 import Home from '../components/Home'
 import NotFound from '../components/404'
+import dhimasf from '../components/dhimasf'
 
 export default function App() {
    return (
@@ -12,6 +13,7 @@ export default function App() {
             <Header/>
             <Switch>
                <Route path="/" exact component={Home}/>
+               <Route path="/dhimasf" component={dhimasf} />
                <Route component={NotFound} />
             </Switch>
          </div>
