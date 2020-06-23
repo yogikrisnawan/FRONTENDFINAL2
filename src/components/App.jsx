9dfd19a85d280873641aa4b35ab434ecde812221
ghalibgendut <ghalibgendut@gmail.com>
2020-06-23 15:08:32 +0700

    Ghalib's Page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Switch }  from 'react-router-dom'
 
 import Header from '../components/Header'
 import Home from '../components/Home'
+import Ghalib from '../components/Ghalib'
 import NotFound from '../components/404'
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
             <Header/>
             <Switch>
                <Route path="/" exact component={Home}/>
+               <Route path="/ghalib" component={Ghalib}/>
                <Route component={NotFound} />
             </Switch>
          </div>
