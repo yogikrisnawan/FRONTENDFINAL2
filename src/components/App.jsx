f35a802c8df9d1800bc375c34408b6cbd388d0c4
rochafialvin <rochafi.dev@gmail.com>
2020-06-23 14:54:22 +0700

    first init

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, BrowserRouter, Switch }  from 'react-router-dom'
+
+import Header from '../components/Header'
+import Home from '../components/Home'
+import NotFound from '../components/404'
+
+export default function App() {
+   return (
+      <BrowserRouter>
+         <div className="center">
+            <Header/>
+            <Switch>
+               <Route path="/" exact component={Home}/>
+               <Route component={NotFound} />
+            </Switch>
+         </div>
+      </BrowserRouter>
+   )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">HOME</NavLink>
+            </NavItem>
+          </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
