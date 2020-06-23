8567bdb15efc80d2e2b0670005aaab7118287933
fadillatif <mfdhllatif@yahoo.com>
2020-06-23 15:08:24 +0700

    fadil

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter, Switch }  from 'react-router-dom'
 import Header from '../components/Header'
 import Home from '../components/Home'
 import NotFound from '../components/404'
+import Fadil from '../components/fadil'
 
 export default function App() {
    return (
@@ -12,6 +13,7 @@ export default function App() {
             <Header/>
             <Switch>
                <Route path="/" exact component={Home}/>
+               <Route path="/fadil" component={Fadil}/>
                <Route component={NotFound} />
             </Switch>
          </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,6 +27,9 @@ const Example = (props) => {
             <NavItem>
               <NavLink href="/">HOME</NavLink>
             </NavItem>
+            <NavItem>
+              <NavLink href="/fadil">Fadil</NavLink>
+            </NavItem>
           </Nav>
       </Navbar>
     </div>
